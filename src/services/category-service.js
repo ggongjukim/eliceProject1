3bac999ba47c1b79c0374f0d5557487c5d6dac1c
SonByungjin <sgyos000@gmail.com>
2022-11-03 10:42:19 +0900

    Feature: dependency injection apply

--- src/services/category-service.js
+++ src/services/category-service.js
@@ -1,27 +1,30 @@
 import { categoryModel } from "../db";
 
 class CategoryService {
+  constructor(categoryModel) {
+    this.categoryModel = categoryModel;
+  }
   async addCategory(categoryInfo) {
-    const createdNewProduct = await categoryModel.create(categoryInfo);
+    const createdNewProduct = await this.categoryModel.create(categoryInfo);
     return createdNewProduct;
   }
 
   async getCategorylist() {
-    const categories = await categoryModel.findAll();
+    const categories = await this.categoryModel.findAll();
     return categories;
   }
 
   async getCategoryById(categoryId) {
-    const category = await categoryModel.findById(categoryId);
+    const category = await this.categoryModel.findById(categoryId);
     return category;
   }
 
   async setCategory(categoryId, name) {
-    const category = await categoryModel.update(categoryId, name);
+    const category = await this.categoryModel.update(categoryId, name);
     return category;
   }
 }
 
-const categoryService = new CategoryService();
+const categoryService = new CategoryService(categoryModel);
 
 export { categoryService };
--- src/services/order-service.js
+++ src/services/order-service.js
@@ -1,28 +1,31 @@
 import { orderModel } from "../db";
 
 class OrderService {
+  constructor(orderModel) {
+    this.orderModel = orderModel;
+  }
   async addOrder(orderInfo) {
-    const newOrder = await orderModel.create(orderInfo);
+    const newOrder = await this.orderModel.create(orderInfo);
     return newOrder;
   }
 
   async getOrderlist() {
-    const orders = await orderModel.findAll();
+    const orders = await this.orderModel.findAll();
     return orders;
   }
 
   async getOrderlistByUserId(userId) {
-    const orders = await orderModel.findByUserId(userId);
+    const orders = await this.orderModel.findByUserId(userId);
     return orders;
   }
 
   async getOrderById(orderId) {
-    const order = await orderModel.findByOrderId(orderId);
+    const order = await this.orderModel.findByOrderId(orderId);
     return order;
   }
 
   async deleteOrder(orderId) {
-    const result = await orderModel.deleteById(orderId);
+    const result = await this.orderModel.deleteById(orderId);
     return result;
   }
 
@@ -32,6 +35,6 @@ class OrderService {
   }
 }
 
-const orderService = new OrderService();
+const orderService = new OrderService(orderModel);
 
 export { orderService };
