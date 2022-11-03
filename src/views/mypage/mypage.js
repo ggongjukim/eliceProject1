6c502cc74c5ad476ad8ac67e9c0cd5f05da8e52a
sak5010 <sak5010@naver.com>
2022-11-03 19:16:20 +0900

    주소 변경 기능 추가

--- src/views/mypage/mypage.js
+++ src/views/mypage/mypage.js
@@ -12,6 +12,8 @@ const userName = document.querySelector("#user-name");
 const userNameButton = document.querySelector("#user-name-button");
 const userEmail = document.querySelector("#user-email");
 const userPasswordButton = document.querySelector("#user-password-button");
+const userAddress = document.querySelector("#user-address");
+const userAddressButton = document.querySelector("#user-address-button");
 const userWithdrawButton = document.querySelector("#user-widthdraw-button");
 
 addAllElements();
@@ -22,12 +24,14 @@ async function addAllElements() {
   insertIsAdmin();
   insertUserName();
   insertUserEmail();
+  insertUserAddress();
 }
 
 // 여러 개의 addEventListener들을 묶어주어서 코드를 깔끔하게 하는 역할임.
 function addAllEvents() {
   userNameButton.addEventListener("click", changeNameHandler);
   userPasswordButton.addEventListener("click", changePasswordHandler);
+  userAddressButton.addEventListener("click", changeAddressHandler);
 }
 
 // admin 유저일 경우 이름 위에 관리자 표시
@@ -54,6 +58,18 @@ async function insertUserName() {
 async function insertUserEmail() {
   const email = localStorage.getItem("email");
   userEmail.innerText = email;
+}
+
+async function insertUserAddress() {
+  try {
+    const email = localStorage.getItem("email");
+    const user = await Api.get(`/api/email/${email}`);
+    const { address } = user;
+    userAddress.innerText = address;
+  } catch (err) {
+    console.error(err.stack);
+    alert(`문제가 발생하였습니다. 확인 후 다시 시도해 주세요: ${err.message}`);
+  }
 }
 
 // 유저 이름 변경
@@ -85,6 +101,30 @@ async function changePasswordHandler(e) {
     const data = { currentPassword, newPassword, email };
     const user = await Api.patch("/api/user", "password", data);
     alert("비밀번호가 성공적으로 변경되었습니다.");
+  } catch (err) {
+    console.error(err.stack);
+    alert(`문제가 발생하였습니다. 확인 후 다시 시도해 주세요: ${err.message}`);
+  }
+}
+
+// 유저 주소 변경
+async function changeAddressHandler(e) {
+  const email = localStorage.getItem("email");
+  const address = prompt("변경할 주소를 입력하세요.");
+  // const isPostCodeValid = postCode.length === 5;
+  const isAddressValid = address.length >= 2;
+  // if (!isPostCodeValid) {
+  //   return alert("우편번호는 숫자 5자리를 입력해 주세요.");
+  // }
+  if (!isAddressValid) {
+    return alert("올바른 주소를 입력해주세요.");
+  }
+  try {
+    const data = { address, email };
+    const user = await Api.patch("/api/user", "address", data);
+    const updatedAddress = user.address;
+    userAddress.innerText = updatedAddress;
+    alert(`이름이 ${updatedAddress}으로 변경되었습니다.`);
   } catch (err) {
     console.error(err.stack);
     alert(`문제가 발생하였습니다. 확인 후 다시 시도해 주세요: ${err.message}`);
