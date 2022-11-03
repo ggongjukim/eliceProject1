848f2e286f81a7e91b72596857da21f680725c79
차지환 <ckwlghks12@gmail.com>
2022-11-03 16:11:38 +0900

    cart.js 이벤트리스너 작성

--- src/views/cart/cart.js
+++ src/views/cart/cart.js
@@ -2,6 +2,8 @@ import * as Api from "../api.js";
 
 const $cartMain = document.querySelector(".cart-main");
 const $cartIn = document.querySelector(".cart-in");
+const $title = document.querySelector(".title");
+const $list = document.querySelector(".product-list");
 
 const token = getStorage("token");
 
@@ -16,7 +18,7 @@ function getStorage(name) {
   return item;
 }
 
-function nonTemplate() {
+function emptyCart() {
   $cartMain.innerHTML = `
   <div class="cart-none">
     <img src="" alt="카트이미지" class="cart-image" />
@@ -25,67 +27,53 @@ function nonTemplate() {
   `;
 }
 
-function genTemplate({ list }) {
+function totalPrice(list) {
+  if (list.length === 0) {
+    return emptyCart();
+  }
+
   const productPrice = list.reduce(
     (acc, item) => acc + item.price * item.amount,
     0
   );
   const shipFee = productPrice < 30000 ? 3000 : 0;
   const total = (productPrice + shipFee).toLocaleString();
-  const items = list.map(
-    ({ name, amount, price, images }) => `
-      <div class="product">
+  const $productPrices = document.querySelectorAll(".product-price");
+  const $totalPrices = document.querySelectorAll(".total-price");
+  const $ship = document.querySelectorAll(".ship");
+
+  $ship.forEach((e) => (e.innerText = `${shipFee.toLocaleString()}원`));
+
+  $productPrices.forEach(
+    (e) => (e.innerText = `${productPrice.toLocaleString()}원`)
+  );
+
+  $totalPrices.forEach((e) => (e.innerText = `${total}원`));
+}
+
+function genProduct(list) {
+  const items = list
+    .map(
+      ({ id, name, amount, price, images }) => `
+      <div class="product" id="${id}">
         <div class="product-section">
-          <img src=${images} alt="" class="product-image" />
+          <img src="${images}" alt="이미지" class="product-image" />
           <div class="product-name">${name}</div>
           <div class="product-stock">재고 있음</div>
         </div>
         <div class="product-amount">
-          <button class="decrease">-</button>
+          <button class="decrease" ${amount === 1 && "disabled"}>-</button>
           <div class="amount">${amount}</div>
-          <button class="decrease">+</button>
+          <button class="increase">+</button>
         </div>
-        <div class="total-price">${(price * amount).toLocaleString()}원</div>
+        <div class="product-total">${(price * amount).toLocaleString()}원</div>
         <div class="section-button">
           <button class="delete">삭제</button>
-          <button class="later">나중에 사기</button>
         </div>
       </div>`
-  );
-
-  const costTemplate = `        
-  <div class="shipping-cost">
-    <div class="price">
-      <span>상품금액</span>
-      <strong>${productPrice.toLocaleString()}원</strong>
-    </div>
-    <div class="shipping">
-      <span>배송비(30,000원 이상 무료)</span>
-      <strong>${shipFee === 3000 ? "3,000원" : "무료"}</strong>
-    </div>
-    <div class="total">
-      <span>주문금액</span>
-      <strong>${total}원</strong>
-    </div>
-  </div>
-  <footer>
-    <div class="footer-price">
-      <span>총 상품금액</span>
-      <strong>${productPrice.toLocaleString()}원</strong>
-    </div>
-    <div class="footer-shipping">
-      <span>총 배송비</span>
-      <strong>+${shipFee === 3000 ? "3,000원" : "무료"}</strong>
-    </div>
-    <div class="footer-total">
-      <span>결제금액</span>
-      <strong>${total}원</strong>
-      <button class="purchase">구매하기</button>
-    </div>
-  </footer>`;
-
-  $cartIn.insertAdjacentHTML("beforeend", items);
-  $cartIn.insertAdjacentHTML("beforeend", costTemplate);
+    )
+    .join("");
+  $list.innerHTML = items;
 }
 
 async function memberCart(type) {
@@ -99,21 +87,82 @@ async function memberCart(type) {
     },
   };
 
-  let data = await memType[type]();
-
-  function setState(newState) {
-    data = newState;
-    genTemplate(data);
-  }
+  let data = (await memType[type]()).list;
 
   if (!data) {
-    nonTemplate();
+    emptyCart();
     return;
   }
 
-  genTemplate(data);
+  genProduct(data);
+  totalPrice(data);
 
-  $cartMain.addEventListener("click", (e) => {});
+  $cartIn.style.display = "block";
+
+  $cartMain.addEventListener("click", ({ target }) => {
+    const parent = target.parentNode.parentNode;
+    const id = parent.id;
+    const decrease = target.parentNode.querySelector(".decrease");
+    const $productTotal = parent.querySelector(".product-total");
+    const productData = data.find((e) => e.id === Number(id));
+
+    switch (target.className) {
+      case "increase":
+        const $elem = target.previousElementSibling;
+        const numAdd = Number($elem.innerText) + 1;
+
+        if (numAdd === 2) decrease.disabled = false;
+
+        $elem.innerText = numAdd;
+
+        data = data.map((e) =>
+          e.id === Number(id) ? { ...e, amount: numAdd } : e
+        );
+
+        $productTotal.innerText = `${(
+          productData.price * numAdd
+        ).toLocaleString()}원`;
+
+        totalPrice(data);
+        // post 요청
+        break;
+
+      case "decrease":
+        const $elem2 = target.nextElementSibling;
+        const numSub = Number($elem2.innerText) - 1;
+
+        if (numSub === 1) target.disabled = true;
+
+        $elem2.innerText = numSub;
+
+        data = data.map((e) =>
+          e.id === Number(id) ? { ...e, amount: numSub } : e
+        );
+
+        $productTotal.innerText = `${(
+          productData.price * numSub
+        ).toLocaleString()}원`;
+
+        totalPrice(data);
+        // post 요청
+        break;
+
+      case "delete":
+        $list.removeChild(parent);
+
+        data = data.filter((e) => e.id !== Number(id));
+        totalPrice(data);
+        // deleted = data.find(e=> e.id === Number(id));
+        // delete 요청
+
+        break;
+      case "puchase-btn":
+        // post 요청
+        break;
+      default:
+        return;
+    }
+  });
 }
 
 token ? memberCart("mem") : memberCart("nonMem");
