8cc6e234f64009d7b9af3adb1ba15dfeba3f3cfa
sak5010 <sak5010@naver.com>
2022-11-05 20:23:05 +0900

    home.js 상단에 navbar에 대한 주석 추가

--- src/views/home/home.js
+++ src/views/home/home.js
@@ -1,6 +1,12 @@
 // 아래는 현재 home.html 페이지에서 쓰이는 코드는 아닙니다.
 // 다만, 앞으로 ~.js 파일을 작성할 때 아래의 코드 구조를 참조할 수 있도록,
 // 코드 예시를 남겨 두었습니다.
+/**
+ * @detail: 지금까지 작성된 코드는 nav와 관련된 코드로, 다른 페이지에서도 현재 js 파일과
+ * home.html파일의 navbar 부분이 사용될 수 있습니다.
+ * @editor: 김상현
+ * @date: 2022-11-05
+ */
 
 import * as Api from "/api.js";
 import { randomId } from "/useful-functions.js";
