12c96bc59b0407a709a4279334b67eadc20658e3
차지환 <ckwlghks12@gmail.com>
2022-11-07 01:24:15 +0900

    Feature: 우편번호 api 적용

--- src/views/order/order.js
+++ src/views/order/order.js
@@ -1,6 +1,12 @@
 import * as Api from "../api.js";
 
 const $productsList = document.querySelector(".products-list");
+const $paymentBtn = document.querySelector("#payment-btn");
+const $userAddress = document.querySelector(".user-address");
+const $zipCode = document.querySelector("#zip-code");
+const $address = document.querySelector("#address");
+const $detailAdress = document.querySelector("#detailed-address");
+const $postCode = document.querySelector("#postcode");
 
 function renderProductsList(products) {
   products.forEach(({ amount, product, _id }) => {
@@ -13,6 +19,7 @@ function renderProductsList(products) {
     const $quantity = document.createElement("div");
     const $priceWrapper = document.createElement("div");
     const $price = document.createElement("strong");
+    const $productInner = document.createElement("div");
 
     $li.classList.add("product-wrapper");
     $imgWrapper.classList.add("product-img-wrapper");
@@ -37,8 +44,9 @@ function renderProductsList(products) {
     $info.appendChild($titleWrapper);
     $info.appendChild($priceWrapper);
     $imgWrapper.appendChild($img);
-    $li.appendChild($imgWrapper);
-    $li.appendChild($info);
+    $li.appendChild($productInner);
+    $productInner.appendChild($imgWrapper);
+    $productInner.appendChild($info);
     $productsList.appendChild($li);
   });
 }
@@ -67,17 +75,58 @@ function getTotalPrice(data) {
   );
 }
 
+function makePopUp(x, y) {
+  const width = 350;
+  const height = 500;
+  const border = 3;
+
+  $postCode.style.width = `${width}px`;
+  $postCode.style.height = `${height}px`;
+  $postCode.style.border = `${border}px solid black`;
+  $postCode.style.position = `absolute`;
+  $postCode.style.left = `${x - 10}px`;
+  $postCode.style.top = `${y - 10}px`;
+}
+
 async function getData() {
   // let data = await Api.get("http://localhost:3000", "api/cart");
   let data = await Api.get(".", "test.json");
-  console.log(data);
 
   renderProductsList(data.list);
 
   const price = getTotalPrice(data.list);
   const total = updateTotalPrice(price);
 
-  console.log(total);
+  $userAddress.addEventListener("click", (e) => {
+    const target = e.target;
+    if (target.className === "zip-address-wrapper") {
+      const [x, y] = [e.pageX, e.pageY];
+      console.log(x, y);
+      new daum.Postcode({
+        oncomplete: function (address) {
+          $zipCode.value = address.zonecode;
+          $address.value = address.address;
+          $detailAdress.focus();
+        },
+        onclose: () => {
+          $postCode.style.display = "none";
+        },
+        width: "100%",
+        height: "100%",
+      }).embed($postCode, {});
+      $postCode.style.display = "block";
+      makePopUp(x, y);
+    }
+  });
 }
+
+$productsList.addEventListener("click", ({ target }) => {
+  if (target.className === "product-img-wrapper") {
+    const id = target.parentNode.parentNode.id;
+    window.location.replace(`/products/${id}`);
+  }
+});
+
+$paymentBtn.addEventListener("click", (e) => {});
 
 getData();
