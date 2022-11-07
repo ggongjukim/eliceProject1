ce756292e64c0f6ecdf18dee39b8c5ac5cb5b5f3
LESANF <nagong1000@naver.com>
2022-11-08 03:22:35 +0900

    PR

--- src/views/productdetail/productdetial.js
+++ src/views/productdetail/productdetial.js
@@ -1,16 +1,16 @@
-import * as Api from '/api.js';
+import * as Api from "/api.js";
 
-const productFrame = document.querySelector('#product-frame');
-const productImg = document.querySelector('#product-image');
-const productCtg = document.querySelector('#ctg');
-const productTitle = document.querySelector('#title');
-const productPrice = document.querySelector('#price');
-const productDes = document.querySelector('#description');
-const productId = location.href.split('/')[4];
-const minusBtn = document.querySelector('#minusBtn');
-const plusBtn = document.querySelector('#plusBtn');
-const amount = document.querySelector('#amountInput');
-const cartBtn = document.querySelector('#pushCart');
+const productFrame = document.querySelector("#product-frame");
+const productImg = document.querySelector("#product-image");
+const productCtg = document.querySelector("#ctg");
+const productTitle = document.querySelector("#title");
+const productPrice = document.querySelector("#price");
+const productDes = document.querySelector("#description");
+const productId = location.href.split("/")[4];
+const minusBtn = document.querySelector("#minusBtn");
+const plusBtn = document.querySelector("#plusBtn");
+const amount = document.querySelector("#amountInput");
+const cartBtn = document.querySelector("#pushCart");
 
 addAllEvents();
 loadProduct(productId);
@@ -29,7 +29,7 @@ async function loadProduct(productId) {
 function handleMinus() {
     let curAmount = +amount.value;
     curAmount -= 1;
-    if (curAmount < 1) return alert('수량은 최소 1개가 필요합니다');
+    if (curAmount < 1) return alert("수량은 최소 1개가 필요합니다");
     amount.value = curAmount;
 }
 
@@ -39,20 +39,21 @@ function handlePlus() {
     amount.value = curAmount;
 }
 
+// create cart
 async function handleCart() {
-    if (confirm('장바구니에 추가하시겠습니까?')) {
+    if (confirm("장바구니에 추가하시겠습니까?")) {
         const cartInfo = {
             product: productId,
             amount: +amount.value,
         };
 
         const createdCart = await Api.post(`/api/productdetail`, cartInfo);
-        if (createdCart === 'success') return alert('장바구니에 추가되었습니다');
+        if (createdCart === "success") return alert("장바구니에 추가되었습니다");
     }
 }
 
 function addAllEvents() {
-    minusBtn.addEventListener('click', handleMinus);
-    plusBtn.addEventListener('click', handlePlus);
-    cartBtn.addEventListener('click', handleCart);
+    minusBtn.addEventListener("click", handleMinus);
+    plusBtn.addEventListener("click", handlePlus);
+    cartBtn.addEventListener("click", handleCart);
 }
