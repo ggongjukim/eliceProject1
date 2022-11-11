919d03f7f53b8cfb49afbddc4b80b9e72c4f249d
LESANF <nagong1000@naver.com>
2022-11-12 00:17:23 +0900

    fix: completed break

--- src/views/orders/orders.js
+++ src/views/orders/orders.js
@@ -94,12 +94,12 @@ async function loadOrderList() {
                                 break;
                             case '배송완료':
                                 changeProcess = COMPLETED;
-                            case '주문취소':
-                                changeProcess = CANCEL;
-                                break;
                                 e.target.parentElement.parentElement
                                     .querySelector('#deleteBtn')
                                     .classList.add('on');
+                                break;
+                            case '주문취소':
+                                changeProcess = CANCEL;
                                 break;
                         }
                     }
